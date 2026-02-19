39cb030eb4e3cd212cf113e3d1564b8140a49180
Letizia Moretti <letizia.moretti@campusdavinci.edu.it>
2026-02-19 09:35:13 +0100

    argoritmi_notevoli_ordinamento_selection

--- argoritmi_notevoli.py
+++ argoritmi_notevoli.py
@@ -1,10 +1,14 @@
-# algoritmo notevole specifico: di ordinamento
+#algoritmo notevole specifico: di ordinamento
 from kallax import *
-lista = [5, 3, 8, 1, 2]
-
-for i in range(0, len(lista) - 1):
-    minimo = minimolista(lista[i:])
-    indice_minimo = lista[i:].index(minimo) + i
-    lista[indice_minimo], lista[i] = lista[i], lista[indice_minimo]
-
+lista=[5,3,8,1,2]
+#print(lista[1:]) stampa una lista senza il primo numero
+for i in range(0,len(lista)-1):
+    #trovo il minimo
+    minimo=minimolista(lista)
+    #trovo lindice del minimo
+    indice_minimo=lista.index(minimo)
+    #sambio
+    lista[indice_minimo]=lista[i]
+    lista[i]=minimo
+    
 print(lista)
