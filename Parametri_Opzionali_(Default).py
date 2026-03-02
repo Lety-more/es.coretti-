dafe1bdcded8c3f8fb6b190a516a5d91930cf093
Letizia Moretti <letizia.moretti@campusdavinci.edu.it>
2026-03-02 11:14:56 +0100

    Parametri_Opzionali_(Default).py

--- Parametri_Opzionali_(Default).py
+++ Parametri_Opzionali_(Default).py
@@ -12,18 +12,40 @@ print(potenza_base2)
 
 
 #2. Crea benvenuto con parametri nome (obbligatorio) e eta (default=18). Stampa "Benvenuto [nome], hai [eta] anni.". Chiamala in entrambi i modi.
-def benvenuto(nume,eta):
+def benvenuto(nome,eta=18):
     print(f"Benvenuto {nome}, hai {eta} anni.")
-
+    
+name=("Johnny")
+age=("17")
+benvenuto(name,age)
+benvenuto(name)
 
 #3. Modifica calcola per avere a default=1. Chiamala senza argomenti.
-
-
+def calcola(a=1,b=2,c=3):
+    return a * b + c
+print(calcola(5))
+print(calcola(5, 4))
+print(calcola()) # Chiamala senza argomenti
 
 #4. Scrivi una funzione media per due numeri, con secondo opzionale=0. Calcola (a + b)/2.
+def mediaNum(a,b=0):
+    media=(a + b)/2
+    return media
 
+num1=10
+num2=4
+mediaTot=mediaNum(num1)
+print(mediaTot)
 
 
 #5. Simulate un "calcolatore IVA": funzione con prezzo e iva (default=22%). Restituisce prezzo + IVA. Discutete casi d'uso.
+def calcolatore_IVA(prezzo,iva=0.22):
+    somma=prezzo + iva
+    return somma
+prezzo=(100)
+iva=(0.30)
+calcolatore=calcolatore_IVA(prezzo)  #iva in default 
+print(calcolatore) 
 
-
+calcolatore=calcolatore_IVA(prezzo,iva) # non usiamo iva default ma gliela diamo 
+print(calcolatore)
