486f9c5214ca3486be04dbb4df22e79738bda369
Letizia Moretti <letizia.moretti@campusdavinci.edu.it>
2026-04-22 19:13:57 +0200

    jarvis.py
    aggiunto confronto

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,6 @@
 # es preparazione verifica: analisi statistica temperature settimanali
 import random # Importa la libreria per generare numeri casuali
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Funzione per riempire la lista di un giorno con 24 temperature (una per ora)
 def registro_tempertature(giorno):
@@ -58,12 +58,24 @@ def crea_istogramma(dati, num_bins=10, titolo="Istogramma", colore="skyblue"):
     plt.ylabel("Frequenza")
     plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.show()
-    
+ 
 def covarianza(giorno1,giorno2):
     calcolo=0
     for i in range(0,24):
         calcolo=calcolo+(giorno1[i]-(sum(giorno1) / len(giorno1)))*(giorno2[i]-(sum(giorno2) / len(giorno2)))
     calcolo=calcolo/24
     return calcolo
-        
-        
+
+def correlazione(cov, dev_std1, dev_std2, nome_confronto):
+    if dev_std1 * dev_std2 != 0:
+        risultato = cov / (dev_std1 * dev_std2)
+    else:
+        risultato = 0
+    print(f"La correlazione di {nome_confronto} è: {risultato:.2f}")
+    if risultato > 0:
+        print(f"Nel confronto di {nome_confronto}: al crescere della prima cresce anche la seconda")
+    elif risultato < 0:
+        print(f"Nel confronto di {nome_confronto}: al crescere della prima decresce la seconda")
+    else:
+        print(f"Nel confronto di {nome_confronto}: non c'è relazione tra le due variabili")
+    return risultato
